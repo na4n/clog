473297d386f3c33b006aad40c3e8994400ee1187
Nathan Annareddy <nathan.annareddy@gmail.com>
2023-12-22 15:53:59 -0600

    linked list implementation

--- log.c
+++ log.c
@@ -4,137 +4,58 @@
 
 #include "log.h"
 
-typedef struct list_struct{
-  data_t item;
-  struct list_struct *next;
-} log_t;
+struct log_linked_list {
+  char *strdat;
+  struct log_linked_list *next;
+} typedef log_t;
 
-static log_t *headptr = NULL;
-static log_t *tailptr = NULL;
 
-char *datparse(data_t data){
-  char *time = ctime(&data.time);
-  removeNewline(time);
-  char *out = calloc(strlen(time)+strlen(data.string)+10, sizeof(char));
-  strcat(out, time);
-  strcat(out, "   --   ");
-  strcat(out, data.string);
-  strcat(out, "\n");
+log_t *HEAD;
+log_t *curr_log;
 
-  return out;
-}
-
-int logprntstr(char* string){
-  return logstr(string, 1);
-}
-
-int logprntdat(data_t data){
-  return logdat(data, 1);
-}
-
-int logstr(char* string, int p){
-  return logdat((data_t){.time = time(NULL), .string = string}, p);
-}
-
-int logdat(data_t data, int p){
-  if(p == 1){
-    char* dpar = datparse(data);
-    fprintf(stdout, "%s", dpar);
-    free(dpar);
+int init(){
+  HEAD = malloc(sizeof(log_t));
+  if(HEAD != NULL){
+    HEAD = (log_t){"", NULL};
   }
-  
-  if(tailptr == NULL){
-    headptr = malloc(sizeof(log_t));
-    tailptr = headptr;
-  }
-  else{
-    tailptr->next = malloc(sizeof(log_t)); 
-    tailptr = tailptr->next;
-  }
-
-  tailptr->item = data;
-  tailptr->next = NULL;
 
   return 0;
 }
 
-void clearlog(void){
-  log_t *next;
-  while(headptr != NULL){
-    next = headptr->next;
-    free(headptr);
-    headptr = next;
-  }
+int add_log(char *txt){
+  curr_log->strdat = txt;
+  curr_log->next = malloc(sizeof(log_t));
+  curr_log = curr_log->next;
 
-  return;
-}
-
-void removeNewline(char *str) {
-  size_t len = strlen(str);
-  if (len > 0 && str[len - 1] == '\n') {
-      str[len - 1] = '\0';
-  }
-}
-
-char *getlog(void){
-  log_t *current = headptr;
-  char* buf = NULL; // = realloc(buf, 0);
-  char *time_string;
-  int last = 0;
-
-  while(current != NULL){
-    if(buf == NULL){
-      buf = calloc(sizeof(char)*(strlen(current->item.string) + strlen(ctime(&(current->item.time))) + 9) + last, sizeof(char));
-    }
-    else{
-      buf = realloc(buf, sizeof(char)*(strlen(current->item.string) + strlen(ctime(&(current->item.time))) + 9) + last);
-    }   
-    last += sizeof(char)*(strlen(current->item.string) + strlen(ctime(&(current->item.time))) + 9) + sizeof(buf);
-
-    time_string = ctime(&current->item.time);
-    removeNewline(time_string);
-
-    strcat(buf, time_string);
-    strcat(buf, "   --   ");
-    strcat(buf, current->item.string);
-    strcat(buf, "\n");
-
-    current = current->next;
-  }
-
-  return buf;
-}
-
-int savelog(char* filename){ 
-  FILE* fp = (filename == NULL) ? stdout : fopen(filename, "w+");
-  
-  char *buf = getlog();
-  fprintf(fp, "%s", buf);
-  if(fp != stdout){
-    fclose(fp);
-  }
-  
-  free(buf);
   return 0;
 }
 
-// int main(){
-//   time_t currentTime;
-//   time(&currentTime);
+int free_log(log_t *head){
+  while(head != NULL){
+    log_t *n = head->next;
+    free(head);
+    head = n;
+  }
 
-//   data_t d;
-//   d.string = "yay";
-//   d.time = currentTime;
+  return 0;
+}
 
-//   data_t b;
-//   b.string = "uhoh";
-//   b.time = currentTime;
+int display_log(log_t *head){
+  while(head != NULL){
+    printf("%s\n", head->txt);
+  }
 
-//   logprntdat(d);
-//   logprntstr("test");
+  return 0;
+}
 
-//   savelog(NULL);
-//   clearlog();
+int main(){
+  add_log("yay");
+  add_log("test2");
 
-//   return 0;
-// }
+  display_log(HEAD);
+
+  free_log(HEAD);
+
+
+  return 1;
+}
